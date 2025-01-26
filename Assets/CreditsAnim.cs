24d0921a4f0d2e99df88e8446798a8cf2713f16a
geb2701 <86382687+geb2701@users.noreply.github.com>
2025-01-26 02:38:30 -0300

    feat: menu principal

--- Assets/CreditsAnim.cs
+++ Assets/CreditsAnim.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using UnityEngine;
 public class CreditsAnim : MonoBehaviour
 {
     public GameObject Wrapper;
     public float to;
     public float duration;
-    //public Ease easeInOutBack;
-    // Start is called before the first frame update
     void Start()
     {
-    DOTween.Init(true, true, LogBehaviour.Verbose).SetCapacity(2200, 10);
-    Wrapper.transform.DOMoveY(to, duration,true).SetEase(Ease.Linear);
+        DOTween.Init(true, true, LogBehaviour.Verbose).SetCapacity(2200, 10);
+        Wrapper.transform.DOMoveY(to, duration).SetEase(Ease.InOutQuad);
 
     }
 
